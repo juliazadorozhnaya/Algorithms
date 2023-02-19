933216934df0d16113289138e69db0fcda133506
Julia <jzadorozhnaya2707@DESKTOP-E3DPQP3>
2023-02-19 19:27:38 +0300

    moveZeroes solved

--- LeetСode/tasks/moveZeroes/main.go
+++ LeetСode/tasks/moveZeroes/main.go
@@ -1,8 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
+	nums := []int{8, 0, 1}
+	moveZeroes(nums)
+	fmt.Print(nums)
 }
 
-func twoSum(nums []int, target int) []int {
+func moveZeroes(nums []int) {
+	if len(nums) == 0 {
+		return
+	}
+	p := 0
+	for i, _ := range nums {
+		if nums[i] != 0 {
+			nums[p], nums[i] = nums[i], nums[p]
+			p++
+		}
+	}
 
 }
