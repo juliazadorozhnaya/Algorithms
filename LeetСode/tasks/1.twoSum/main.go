420d042462a31ed67795bc5bc5983259bf8936b9
y.zadorozhnaya <j.zadorozhnaya2707@gmail.com>
2024-02-09 10:09:02 +0300

    feat: moveZeroes problem solved

--- LeetСode/tasks/1.twoSum/main.go
+++ LeetСode/tasks/1.twoSum/main.go
@@ -14,7 +14,7 @@ func twoSum(nums []int, target int) []int {
 }
 
 func main() {
-	nums := []int{2, 7, 11, 15}
+	nums := []int{7, 2, 11, 15}
 	var target = 18
 	fmt.Println(twoSum(nums, target))
 }
--- LeetСode/tasks/283.moveZeroes/main.go
+++ LeetСode/tasks/283.moveZeroes/main.go
@@ -2,23 +2,18 @@ package main
 
 import "fmt"
 
-func main() {
-	nums := []int{8, 0, 0, 1}
-	moveZeroes(nums)
-	fmt.Println(nums)
-}
-
 func moveZeroes(nums []int) {
-	if len(nums) == 0 {
-		return
-	}
 	p := 0
 	for i, _ := range nums {
 		if nums[i] != 0 {
-			fmt.Println(i, p)
 			nums[p], nums[i] = nums[i], nums[p]
 			p++
 		}
 	}
+}
 
+func main() {
+	nums := []int{0}
+	moveZeroes(nums)
+	fmt.Println(nums)
 }
